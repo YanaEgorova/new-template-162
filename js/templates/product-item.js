70348b7ad489c1a4b69cc48d128b55f0aecacc2f
Yana <yanaegorova69.3@gmail.com>
2022-09-29 01:23:44 +0300

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -19,29 +19,10 @@ export const item = (product) => {
       $${(product.price).toFixed(2)}
       </span>
       <div class="product__btn-block">
-      <a href="product-page.html?id=${product.id}" class="btn btn--first">
-      details
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                	  viewBox="0 0 220.682 220.682" style="enable-background:new 0 0 220.682 220.682;"
-                	 xml:space="preserve">
-                <g>
-                	<polygon points="92.695,38.924 164.113,110.341 92.695,181.758 120.979,210.043 220.682,110.341 120.979,10.639 	" fill="#000"/>
-                	<polygon points="28.284,210.043 127.986,110.341 28.284,10.639 0,38.924 71.417,110.341 0,181.758 	" fill="#ffa500"/>
-                </g>
-                </svg>
-      </a>
-  
-    <button class="btn js_add-to-cart">
-    buy
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    viewBox="0 0 220.682 220.682" style="enable-background:new 0 0 220.682 220.682;"
-   xml:space="preserve">
-<g>
-  <polygon points="92.695,38.924 164.113,110.341 92.695,181.758 120.979,210.043 220.682,110.341 120.979,10.639 	" fill="#000"/>
-  <polygon points="28.284,210.043 127.986,110.341 28.284,10.639 0,38.924 71.417,110.341 0,181.758 	" fill="#ffa500"/>
-</g>
-</svg>
-    </button>
+      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 220.682 220.682" style="enable-background:new 0 0 220.682 220.682;" xml:space="preserve"><g><polygon points="92.695,38.924 164.113,110.341 92.695,181.758 120.979,210.043 220.682,110.341 120.979,10.639 	" fill="#000"/><polygon points="28.284,210.043 127.986,110.341 28.284,10.639 0,38.924 71.417,110.341 0,181.758 	" fill="#ffa500"/></g></svg></a>
+      ${
+        product.isItRing ? '' : '<button class="btn js_add-to-cart">buy<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 220.682 220.682" style="enable-background:new 0 0 220.682 220.682;" xml:space="preserve"><g><polygon points="92.695,38.924 164.113,110.341 92.695,181.758 120.979,210.043 220.682,110.341 120.979,10.639 	" fill="#000"/><polygon points="28.284,210.043 127.986,110.341 28.284,10.639 0,38.924 71.417,110.341 0,181.758 	" fill="#ffa500"/></g></svg></button>'
+      }
     </div>
       </div>
 
